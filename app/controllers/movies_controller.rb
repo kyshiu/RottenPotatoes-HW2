2fdef02f97f8c5d92505cc7a18a19bb22007c546
Kenny Shiu <k.y.shiu@berkeley.edu>
2012-02-04 09:44:11 -0800

    Movies title has link, not working yet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,11 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params.has_key?(:sortTitle)
+      @movies = Movies.find(:all,:order => title)
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
